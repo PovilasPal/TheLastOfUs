76df269c369865caf35b423d3b933e19f45be806
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-09 14:47:09 +0300

    UserProviderRequestDTO fields validation

--- Back-End/DentistryService/src/main/java/lt/techin/DentistryService/dto/userProvider/UserProviderRequestDTO.java
+++ Back-End/DentistryService/src/main/java/lt/techin/DentistryService/dto/userProvider/UserProviderRequestDTO.java
@@ -1,16 +1,29 @@
 package lt.techin.DentistryService.dto.userProvider;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lt.techin.DentistryService.model.Role;
 
 import java.util.List;
 
 public record UserProviderRequestDTO(
+        @NotNull
+        @Pattern(regexp = "^[A-Z]{3}[0-9]{3}$", message = "Licence number must be of three uppercase letters and 5 digits")
         String licenceNumber,
+        @NotNull
         String name,
+        @NotNull
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$")
         String email,
+        @NotNull
+        @Pattern(regexp = "^+d{1,3}\\d{9}$")
         String phoneNumber,
+        @NotNull
         String username,
+        @NotNull
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.* ).{8,16}$", message = "Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be 8-16 characters long.")
         String password,
+        @NotNull
         List<Role> roles
 ) {
 }
