9ed986f4693b8d1dd668801fdafaa70fdc2ea892
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-10 20:17:17 +0300

    Created UserProviderController

---
+++ Back-End/DentistryService/src/main/java/lt/techin/DentistryService/controller/UserProviderController.java
@@ -0,0 +1,51 @@
+package lt.techin.DentistryService.controller;
+
+import jakarta.validation.Valid;
+import lt.techin.DentistryService.dto.userProvider.UserProviderMapper;
+import lt.techin.DentistryService.dto.userProvider.UserProviderRequestDTO;
+import lt.techin.DentistryService.model.UserProvider;
+import lt.techin.DentistryService.service.UserProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class UserProviderController {
+  private final UserProviderService userProviderService;
+
+  @Autowired
+  public UserProviderController(UserProviderService userProviderService) {
+    this.userProviderService = userProviderService;
+  }
+
+
+  @PostMapping("/providerRegistration")
+  public ResponseEntity<Object> saveUserProvider(
+          @Valid @RequestBody UserProviderRequestDTO userProviderRequestDTO) {
+
+    if (this.userProviderService.existsByname(userProviderRequestDTO.name())) {
+      Map<String, String> response = new HashMap<>();
+      response.put("name", "Already exists");
+      return ResponseEntity.badRequest().body(response);
+    }
+    UserProvider savedUserProvider = this.userProviderService.saveUserProvider(UserProviderMapper.toUserProvider(userProviderRequestDTO));
+
+
+    return ResponseEntity.created(
+                    ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(savedUserProvider.getLicenceNumber())
+                            .toUri())
+            .body(savedUserProvider);
+
+  }
+
+}
