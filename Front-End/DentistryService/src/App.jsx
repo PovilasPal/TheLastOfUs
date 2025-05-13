77461013d94fdb045a574b655c69768dc4a03a08
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-13 10:48:42 +0300

    Add routes to App.jsx

--- Front-End/DentistryService/src/App.jsx
+++ Front-End/DentistryService/src/App.jsx
@@ -1,9 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import UserProviderRegistration from './components/UserProviderRegistrationForm'
+import Navigation from './components/Navigation'
+
+
 function App() {
 
   return (
-    <>
+    <Router>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<UserProviderRegistration/>}/>
+        
+      </Routes>
      
-    </>
+    </Router>
   )
 }
 
