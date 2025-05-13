a8fc2ea6607e990cc67bbf5c6211f3403807c12a
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-13 20:28:57 +0300

    Create Home component

--- Front-End/DentistryService/src/App.jsx
+++ Front-End/DentistryService/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import UserProviderRegistration from './components/UserProviderRegistrationForm'
 import Navigation from './components/Navigation'
+import Home from './components/Home'
 
 
 function App() {
@@ -11,10 +12,10 @@ function App() {
       <Navigation/>
   
       <Routes>
-        
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<UserProviderRegistration/>}/>
         <Route path="/registration-success" element={<h1>Registration successful</h1>}/>
-        <Route path="/*/*" element={<h1>404 Not Found</h1>}/>
+        <Route path="/*" element={<h1>404 Not Found</h1>}/>
       </Routes>
      
   </>
