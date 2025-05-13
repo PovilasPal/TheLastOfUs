628c6cd9757a57ead90dafa57f91a59a9dcf29a1
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-13 20:29:52 +0300

    Create CorsConfig class

--- Back-End/DentistryService/src/main/java/lt/techin/DentistryService/DentistryServiceApplication.java
+++ Back-End/DentistryService/src/main/java/lt/techin/DentistryService/DentistryServiceApplication.java
@@ -2,9 +2,6 @@ package lt.techin.DentistryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DentistryServiceApplication {
@@ -13,16 +10,5 @@ public class DentistryServiceApplication {
     SpringApplication.run(DentistryServiceApplication.class, args);
   }
 
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
-      }
-    };
-  }
+
 }
