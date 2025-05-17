0c816e9861d28588e9a544de706011ee23993cc8
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-17 17:52:19 +0300

    create EmployeeRepository

---
+++ Back-End/DentistryService/src/main/java/lt/techin/DentistryService/repository/EmployeeRepository.java
@@ -0,0 +1,20 @@
+package lt.techin.DentistryService.repository;
+
+import lt.techin.DentistryService.model.Employee;
+import lt.techin.DentistryService.model.UserProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+  Optional<Employee> findByIdAndUserProviderLicenceNumber(Long id, String licenceNumber);
+
+
+  List<Employee> findByUserProviderLicenceNumberAndIsActiveTrue(String licenceNumber);
+
+  boolean existsByLicenceNumber(String licenceNumber);
+
+  Optional<Employee> findByIdAndUserProvider(Long employeeId, UserProvider provider);
+
+}
