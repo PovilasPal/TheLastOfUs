f31dc7d683aea551169e8be5ce5e7d0cf4788945
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-18 13:34:01 +0300

    Add RoleRepository and PasswordEncoder to UserProviderService

--- Back-End/DentistryService/src/main/java/lt/techin/DentistryService/service/UserProviderService.java
+++ Back-End/DentistryService/src/main/java/lt/techin/DentistryService/service/UserProviderService.java
@@ -1,22 +1,31 @@
 package lt.techin.DentistryService.service;
 
+import lt.techin.DentistryService.model.Role;
 import lt.techin.DentistryService.model.UserProvider;
+import lt.techin.DentistryService.repository.RoleRepository;
 import lt.techin.DentistryService.repository.UserProviderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserProviderService {
 
   private final UserProviderRepository userProviderRepository;
+  private final RoleRepository roleRepository;
+  private final PasswordEncoder passwordEncoder;
 
   @Autowired
-  public UserProviderService(UserProviderRepository userProviderRepository) {
+  public UserProviderService(UserProviderRepository userProviderRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
     this.userProviderRepository = userProviderRepository;
+    this.roleRepository = roleRepository;
+    this.passwordEncoder = passwordEncoder;
   }
 
   public Optional<UserProvider> findByUsername(String username) {
@@ -24,10 +33,17 @@ public class UserProviderService {
   }
 
   public UserProvider saveUserProvider(UserProvider userProvider) {
-
     if (userProviderRepository.existsByUsername(userProvider.getUsername())) {
       throw new DuplicateKeyException("Username already exists");
     }
+
+    // Encode password here
+    String encodedPassword = passwordEncoder.encode(userProvider.getPassword());
+    userProvider.setPassword(encodedPassword);
+
+    Role providerRole = roleRepository.findByName("ROLE_PROVIDER");
+    userProvider.setRoles(new ArrayList<>(Set.of(providerRole)));
+
     return userProviderRepository.save(userProvider);
   }
 
