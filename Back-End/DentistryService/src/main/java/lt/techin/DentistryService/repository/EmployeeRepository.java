04be38a3a9c41ed7790e23c357e181edcdf81240
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-20 11:42:12 +0300

    Create EmployeeRepository

---
+++ Back-End/DentistryService/src/main/java/lt/techin/DentistryService/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package lt.techin.DentistryService.repository;
+
+import lt.techin.DentistryService.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
+  List<Employee> findByProviderLicenseNumber(String licenseNumber);
+}
+
