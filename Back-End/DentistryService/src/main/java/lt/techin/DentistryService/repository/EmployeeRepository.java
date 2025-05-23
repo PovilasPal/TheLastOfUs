5870b432be270b8c300d0c47fd06cd5a0b04cfef
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-23 19:53:30 +0300

    Refactor EmployeeRepository.java

--- Back-End/DentistryService/src/main/java/lt/techin/DentistryService/repository/EmployeeRepository.java
+++ Back-End/DentistryService/src/main/java/lt/techin/DentistryService/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package lt.techin.DentistryService.repository;
 
 import lt.techin.DentistryService.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,10 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
   List<Employee> findByProviderLicenseNumber(String licenseNumber);
-}
 
+  @Query("SELECT DISTINCT e FROM Employee e " +
+          "LEFT JOIN FETCH e.treatments " +
+          "LEFT JOIN FETCH e.appointments " +
+          "WHERE e.provider.licenseNumber = :providerLicenseNumber")
+  List<Employee> findByProviderLicenseNumberWithDetails(@Param("providerLicenseNumber") String providerLicenseNumber);
+}
