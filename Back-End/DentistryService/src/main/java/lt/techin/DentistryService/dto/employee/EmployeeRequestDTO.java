25245ddba33011d87c0c38b94082b209e527bd6e
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-23 19:57:06 +0300

    Refactor EmployeeRequestDTO.java

--- Back-End/DentistryService/src/main/java/lt/techin/DentistryService/dto/employee/EmployeeRequestDTO.java
+++ Back-End/DentistryService/src/main/java/lt/techin/DentistryService/dto/employee/EmployeeRequestDTO.java
@@ -1,13 +1,9 @@
 package lt.techin.DentistryService.dto.employee;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 public record EmployeeRequestDTO(
@@ -28,22 +24,8 @@ public record EmployeeRequestDTO(
         List<Long> treatmentIds,
 
 
-        List<@Valid AppointmentDTO> appointments
-
-) {
-  public record AppointmentDTO(
-
-          @NotNull(message = "Appointment date is mandatory")
-          @FutureOrPresent(message = "Appointment date cannot be in the past")
-          LocalDate date,
-
-          @NotNull(message = "Start time is mandatory")
-          LocalTime startTime,
-
-          @NotNull(message = "End time is mandatory")
-          LocalTime endTime
-
-  ) {
-  }
+        List<EmployeeResponseDTO.AppointmentDTO> appointments) {
 }
+
+
 
