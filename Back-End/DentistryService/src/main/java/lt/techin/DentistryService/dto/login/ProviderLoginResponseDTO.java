3c4139317fc8eaad116b51baaf5998cc1b3ae704
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-24 10:10:53 +0300

    Remove deleted files from feature branch

--- Back-End/DentistryService/src/main/java/lt/techin/DentistryService/dto/login/ProviderLoginResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package lt.techin.DentistryService.dto.login;
-
-import java.util.List;
-
-
-public record ProviderLoginResponseDTO(String username, List<String> roles, String licenseNumber) {
-}
-
-
