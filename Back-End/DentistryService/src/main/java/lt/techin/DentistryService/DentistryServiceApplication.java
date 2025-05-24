80ccd1df64e13e79a3aac85d3a72fb02d5b6684c
[Ruta} <[rutalazauskiene1@gmail.com]>
2025-05-24 11:17:47 +0300

    Restore deleted backend files from main without losing feature branch commits

---
+++ Back-End/DentistryService/src/main/java/lt/techin/DentistryService/DentistryServiceApplication.java
@@ -0,0 +1,14 @@
+package lt.techin.DentistryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentistryServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DentistryServiceApplication.class, args);
+  }
+
+
+}
